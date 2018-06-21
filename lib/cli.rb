89bc74c86f79cb79ac1915b0fa3ee82afd612025
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-21 21:46:31 +0000

    added bin and lib folders

---
+++ lib/cli.rb
@@ -0,0 +1,5 @@
+require_relative "../lib/event_scraper.rb"
+require_relative "../lib/event.rb"
+require_relative "../lib/library.rb"
+require 'nokogiri'
+require 'open-uri'
---
+++ lib/event_scraper.rb
@@ -0,0 +1,5 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class 
