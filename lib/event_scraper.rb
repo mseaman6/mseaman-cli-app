164209dc5c5f31fcce44eaf892faa77b97d7a0db
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-22 14:15:07 +0000

    changes to scraper, and addition of attr_accessor

--- lib/event_scraper.rb
+++ lib/event_scraper.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 class Event_Scraper
 
+  attr_accessor :events
 #scrape event information from the library: event_title, event_time, event_description
   def self.scrape_library_page(library_url = "https://www.dclibrary.org/northeast")
     doc = Nokogiri::HTML(open(library_url))
@@ -11,9 +12,9 @@ class Event_Scraper
     binding.pry
     events_info.collect do |event_info|
       event = {
-        name: events_info.css(".field-name-title").text,
-        date: events_info.css(".date-display-single").text,
-        url: events_info.css(".field-name-title").css("a").attribute("href").value
+        name: event_info.css(".field-name-title").text,
+        date: event_info.css(".date-display-single").text,
+        url: event_info.css(".field-name-title").css("a").attribute("href").value
       }
     end
   end
