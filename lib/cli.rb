dfaab2b28416522691728756592054d6756a5b2f
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-22 15:12:01 +0000

    create CLI class, and run and make_events methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,3 +3,17 @@ require_relative "../lib/event.rb"
 require_relative "../lib/library.rb"
 require 'nokogiri'
 require 'open-uri'
+
+class CLI
+
+  def run
+    make_events
+  end
+
+  def make_events
+    events_array = Event_Scraper.scrape_library_page
+    Event.create_from_collection(events_array)
+  end
+
+
+end
