0f280f88cc7fa93eadce2a5e15133a6f7f1ac1d4
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-22 13:11:26 +0000

    updates to scraping text

--- lib/event_scraper.rb
+++ lib/event_scraper.rb
@@ -8,13 +8,15 @@ class Event_Scraper
   def self.scrape_library_page(library_url = "https://www.dclibrary.org/northeast")
     events = []
     doc = Nokogiri::HTML(open(library_url))
-    events_info = doc.css(".group-right")
+    events_info = doc.css(".view-content")
     events_info.each do |event_info|
       event = {
         name: doc.css(".field-name-title").text,
-        date: doc.css("date-display-single").text,
-        url: doc.css(".field-name-title").attribute("href").value
+        date: doc.css(".date-display-single").text,
+        url: doc.css(".field-name-title.a").attribute("href").value
       }
+      binding.pry
+    end
     events << event
   end
 
