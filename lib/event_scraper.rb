9ebc7232ad0b4a0799bc5d62926663bc27981eb6
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-22 14:36:14 +0000

    updates to scraper to try to exclude blog posts

--- lib/event_scraper.rb
+++ lib/event_scraper.rb
@@ -4,13 +4,12 @@ require 'pry'
 
 class Event_Scraper
 
-  attr_accessor :events
+
 #scrape event information from the library: event_title, event_time, event_description
   def self.scrape_library_page(library_url = "https://www.dclibrary.org/northeast")
     doc = Nokogiri::HTML(open(library_url))
-    events_info = doc.css(".view-content").css(".views-row")
-    binding.pry
-    events_info.collect do |event_info|
+    events_info = doc.css(".field-name-events-list").css(".view-content").css(".views-row")
+    events = events_info.collect do |event_info|
       event = {
         name: event_info.css(".field-name-title").text,
         date: event_info.css(".date-display-single").text,
