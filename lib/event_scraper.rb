fccdfdcf79b6c1c2bc97cc42e81da77e76029695
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-22 17:07:15 +0000

    cleaned up comments

--- lib/event_scraper.rb
+++ lib/event_scraper.rb
@@ -4,8 +4,6 @@ require 'pry'
 
 class Event_Scraper
 
-
-#scrape event information from the library: event_title, event_time, event_description
   def self.scrape_library_page(library_url = "https://www.dclibrary.org/northeast#eventsTab")
     doc = Nokogiri::HTML(open(library_url))
     events_info = doc.css(".field-name-events-list").css(".view-content").css(".views-row")
@@ -18,9 +16,6 @@ class Event_Scraper
     end
   end
 
-  #library_url = "https://www.dclibrary.org/northeast#eventsTab"
-
-  #also want to collect the entire description using the event url
   def self.scrape_event_page(event_url)
     doc = Nokogiri::HTML(open(event_url))
     event_info = {
