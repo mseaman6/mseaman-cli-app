d526433142736a42e7831ac8d7f748c27abdf5c2
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-26 12:45:45 +0000

    initial stab at interface method

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,7 +10,6 @@ class CLI
   def run
     make_events
     add_event_description
-    binding.pry
   end
 
   def make_events
@@ -22,6 +21,21 @@ class CLI
     Event.all.each do |event|
       event_description = Event_Scraper.scrape_event_page("https://www.dclibrary.org" + event.url)
       event.add_event_attributes(event_description)
+    end
+  end
+
+  def interface
+    puts "Welcome to the DC Library event lister!"
+    puts "To see a list of the upcoming events at the Northeast Library, enter 'events'."
+    puts "To quit, please enter 'exit'."
+    puts "What would you like to do?"
+    user_input = gets.strip.downcase
+    if user input == "events"
+      (event list) - create method
+    elsif user_input == "exit"
+      break
+    else
+      puts "I did not understand your selection.  Please try again."
     end
   end
 end
